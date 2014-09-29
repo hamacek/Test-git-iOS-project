810f38d6572d3a4c9bdbbaec9071462f361e698b
Luk <luk@Luks-Mac.local>
2014-09-29 15:22:35 +0200

    added a test

---
+++ Git test/GTTest.h
@@ -0,0 +1,13 @@
+//
+//  GTTest.h
+//  Git test
+//
+//  Created by Luk on 29/09/14.
+//  Copyright (c) 2014 Project soft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTest : NSObject
+
+@end
---
+++ Git test/GTTest.m
@@ -0,0 +1,13 @@
+//
+//  GTTest.m
+//  Git test
+//
+//  Created by Luk on 29/09/14.
+//  Copyright (c) 2014 Project soft. All rights reserved.
+//
+
+#import "GTTest.h"
+
+@implementation GTTest
+
+@end
